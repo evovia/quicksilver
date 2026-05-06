18f954a8a9808458e809358b1a6f917534a0334a
Nicolai Bach Woller <woller@traels.it>
2026-05-06 21:22:09 +0200

    Feature. Require stylesheets from engine.

--- quicksilver-ui/lib/quicksilver_ui/engine.rb
+++ quicksilver-ui/lib/quicksilver_ui/engine.rb
@@ -5,5 +5,9 @@ module QuicksilverUI
     isolate_namespace QuicksilverUI
 
     config.autoload_paths << root.join("app", "helpers").to_s
+
+    initializer "quicksilver_ui.assets" do |app|
+      app.config.assets.paths << QuicksilverUI.stylesheets_path.to_s
+    end
   end
 end
